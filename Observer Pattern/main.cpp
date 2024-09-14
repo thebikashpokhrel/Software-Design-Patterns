ad3827e33766ce2acbe4503f67f9fe5f98f7b7f6
Bikash <thebikashpokhrel@gmail.com>
2024-09-14 18:17:15 +0545

    worked more on observer pattern

--- Observer Pattern/main.cpp
+++ Observer Pattern/main.cpp
@@ -3,6 +3,7 @@ Designing a weather station based on Observer Pattern
 */
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <memory>
 
@@ -67,11 +68,81 @@ public:
         notifyObservers();
     }
 
-    void setMeasurements(float pressure, float temperature, float humidity)
+    void setMeasurements(float pres, float temp, float humd)
     {
-        pressure = pressure;
-        temperature = temperature;
-        humidity = humidity;
+        pressure = pres;
+        temperature = temp;
+        humidity = humd;
         measurementsChanged();
     }
 };
+
+// Concrete Display Class : currentConditionsDisplay
+class currentCondtionsDisplay : public IDisplayElement, IObserver
+{
+    float temperature;
+    float humidity;
+    ISubject *weatherData;
+
+public:
+    currentCondtionsDisplay(ISubject *wd)
+    {
+        weatherData = wd;
+        weatherData->registerObserver(this);
+    }
+
+    void update(float pres, float temp, float humd)
+    {
+        temperature = temp;
+        humidity = humd;
+        display();
+    }
+
+    void display()
+    {
+        std::cout << "Current conditions: Temperature = " << temperature << " deg F and humidity = " << humidity << std::endl;
+    }
+};
+
+// Concrete Display Class: HeatIndex Display
+class heatIndexDisplay : public IDisplayElement, IObserver
+{
+    float heatIndex;
+    ISubject *weatherData;
+
+    float computeHeatIndex(float t, float rh)
+    {
+        return -42.379 + 2.04901523 * t + 10.14333127 * rh - 0.22475541 * t * rh - 0.00683783 * t * t - 0.05481717 * rh * rh + 0.00122874 * t * t * rh + 0.00085282 * t * rh * rh - 0.00000199 * t * t * rh * rh;
+    }
+
+public:
+    heatIndexDisplay(ISubject *wd)
+    {
+        weatherData = wd;
+        weatherData->registerObserver(this);
+    }
+
+    void update(float presure, float temp, float humidity) override
+    {
+        heatIndex = computeHeatIndex(temp, humidity);
+        display();
+    }
+
+    void display()
+    {
+        std::cout << "Current heat index is: " << heatIndex << std::endl;
+    }
+};
+
+// Main function
+int main()
+{
+    WeatherData *weatherData = new WeatherData();        // New weather data object
+    currentCondtionsDisplay currentDisplay(weatherData); // New current display object
+    heatIndexDisplay HIDisplay(weatherData);
+
+    // Setting new measurements automatically notify observers and data is printed
+    weatherData->setMeasurements(20.4, 98.3, 70);
+    weatherData->setMeasurements(20.4, 98.3, 65);
+    return 0;
+}
